b914b61398c7191f6e5bd8c4ea107c37c6608116
Hannon Mulaudzi <hannonmulaudzi@gmail.com>
2022-07-07 10:55:13 +0200

    initialize

---
+++ pizza-cart-api.js
@@ -0,0 +1,23 @@
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('pizzaCartWithAPIWidget', function() {
+      return {
+        init() {
+
+        axios.
+       get('https://pizza-cart-api.herokuapp.com/api/pizzas' )
+       .then ((result) => {
+        //console.log(result.data);
+        this.pizzas = result.data.pizzas
+       })
+        },
+        
+        message : 'Eating pizzas',
+        pizzas : [],
+
+        add(pizza) {
+            alert(pizza.flavour + " : " + pizza.size)
+        }
+      }
+    });
+})
